4db38171902a395685f580e50e40aea6beb66b19
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-08-22 15:00:42 -0300

    22/08/2022

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,23 +1,20 @@
-import sys
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 import pygame
 def run_game():
     # Inicaliza o pygame, as configurações e o objeto screen
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_height, ai_settings.screen_height)
+        (ai_settings.screen_width, ai_settings.screen_height)
         )
     pygame.display.set_caption("Alien Invasion")
+    # Cria uma espaçonave
+    ship = Ship(screen)
     # Inicia o laço principal do jogo
     while True:
-        # Observa eventosde teclado e de mouse
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # Redesenha a tela  a cada passagem pelo laço
-        screen.fill(ai_settings.bg_color)
-        # Deixa a tela mais recente visível
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
